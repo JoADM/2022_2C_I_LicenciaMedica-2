bda7b7c434f323c8003b857f16fbe7861a0efd53
JoADM <83179068+JoADM@users.noreply.github.com>
2022-10-17 20:30:22 -0300

    Arreglo error y cambio de nombre al proyecto

---
+++ Controllers/MedicosController.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LicenciaMedica.Models;
+
+namespace LicenciaMedica.Controllers
+{
+    public class MedicosController : Controller
+{
+    public IActionResult Medicos()
+    {
+        return View();
+    }
+}
+}
---
+++ Views/Medicos/Medicos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LicenciaMedica.Controllers
+{
+    public class pruebaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
